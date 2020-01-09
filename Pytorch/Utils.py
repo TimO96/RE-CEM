b16c84e7974cce6a653da6435c6cc1a05acab8c8
Mattijs Blankesteijn <25477219+MBWhitestone@users.noreply.github.com>
2020-01-09 15:33:57 +0100

    for debugging

--- Pytorch/Utils.py
+++ Pytorch/Utils.py
@@ -20,9 +20,13 @@
 ## (C) 2020 Changes by UvA FACT AI group [Pytorch conversion]
 
 from torch.nn import Module, Conv2d, LeakyReLU, MaxPool2d, Upsample, Sequential
-from torch import save, eye, uint8
+from torch import load, save, eye, uint8, from_numpy
+# import torch
+from torchsummary import summary
 
 import os
+import h5py
+import numpy as np
 
 class Print(Module):
     def __init__(self):
@@ -49,10 +53,7 @@ class AE(Module):
         self.filter_size = 16
         self.padding = 1
 
-        model = [
-            #
-            # Encoder
-            #
+        self.encoder = Sequential(
             Conv2d(self.num_channels, self.filter_size, self.kernel_size,
                    padding=self.padding),
             LeakyReLU(self.relu_slope),
@@ -62,20 +63,19 @@ class AE(Module):
             MaxPool2d(self.pool_kernel_size),
             Conv2d(self.filter_size, self.num_channels, self.kernel_size,
                    padding=self.padding),
-            #
-            # Decoder
-            #
+        )
+
+        self.decoder = Sequential(
             Conv2d(self.num_channels, self.filter_size, self.kernel_size,
                    padding=self.padding),
             LeakyReLU(self.relu_slope),
             Upsample(scale_factor=self.pool_kernel_size),
-            # Upsample(size=self.pool_kernel_size),
             Conv2d(self.filter_size, self.filter_size, self.kernel_size,
                    padding=self.padding),
             LeakyReLU(self.relu_slope),
             Conv2d(self.filter_size, self.num_channels, self.kernel_size,
                    padding=self.padding),
-        ]
+        )
 
         if restore:
             try:
@@ -85,47 +85,98 @@ class AE(Module):
                 print("Make sure restore is a torch.load(PATH) object")
                 raise
 
-        self.model = Sequential(*model)
+        # self.model = Sequential(*model)
 
     def predict(self, data):
         """Predict output for input data (batch, dim1, dim2, c)."""
         assert data[0].shape == (28, 28, 1), "Expected shape (b, 28, 28, 1)."
 
         # Reshape data, expect (batch, channel, dim1, dim2)
-        pred = self.model(data.view(-1, 1, self.image_size, self.image_size))
+        data = data.view(-1, 1, self.image_size, self.image_size)
+        latent = self.encoder(data)
+        pred = self.decoder(latent)
 
         return pred.view(-1, self.image_size, self.image_size, 1)
 
-m = AE()
-print(m)
-from setup_mnist import *
-data = MNIST()
-p = m.predict(data.test_data[0:10])
-print(p.shape)
+    def forward(self, data):
+        """Predict alias."""
+        return self.predict(data)
+# m = AE()
+# print(m)
+# from setup_mnist import *
+# data = MNIST()
+# p = m.predict(data.test_data[0:10])
+# print(p.shape)
+# print(m.state_dict().keys())
+# for para in m.parameters():
+#     print(para.shape)
+# save(m.state_dict(), 'state_dict')
+#
+# v = AE(load('state_dict'))
 
-def load_AE(codec_prefix, print_summary=False, dir="models/AE_codec/"):
-    """Load autoencoder from json, optionally print summary."""
-    # Decoder files
-    prefix = dir + codec_prefix + "_"
-    decoder_model_file = prefix + "decoder.json"
-    decoder_weight_file = prefix + "decoder.h5"
+# def find(name, item):
+#     # print(item)
+#     # try:
+#     if isinstance(item, h5py.Dataset):
+#         # print(item.parent.file)
+#         # print(name)
+#         s = item.shape
+#         print(name)
+#         print(name.split('/')[1:], s)
+#         # print(item.file)
+#         # print(item.name)
+#         # print(item.parent)
+#         # print(item.shape)
+#         # return s
+#         # d[name]=s
+#     # except:/
+#         # pass
 
-    if not os.path.isfile(decoder_model_file):
-        raise Exception(f"Decoder model file {decoder_model_file} not found.")
-    if not os.path.isfile(decoder_weight_file):
-        raise Exception(f"Decoder weight file {decoder_weight_file} not found.")
+def h5_to_state_dict(h5_file, mapping):
+    """Create Pytorch state_dict from h5 weight with mapping."""
+    state_dict = {}
+    with h5py.File(h5_file, 'r') as f:
+        for h5, state in mapping.items():
+            state_dict[state] = from_numpy(f[h5][:].T)
+    return state_dict
 
-    json_file = open(decoder_model_file, 'r')
-    decoder = model_from_json(json_file.read(), custom_objects={"tf": tf})
-    json_file.close()
+def load_AE(codec_prefix, print_summary=False, dir="models/"):
+    """Load autoencoder from json. Optionally print summary"""
+    # Weight file
+    weight_file = dir + codec_prefix  + ".h5"
+    if not os.path.isfile(weight_file):
+        raise Exception(f"Decoder weight file {weight_file} not found.")
 
-    decoder.load_weights(decoder_weight_file)
+    # AE mapping
+    AE_map = {
+        'conv2d_4/conv2d_4/bias:0'       : 'decoder.0.bias',
+        'conv2d_4/conv2d_4/kernel:0'     : 'decoder.0.weight',
+        'conv2d_5/conv2d_5/bias:0'       : 'decoder.3.bias',
+        'conv2d_5/conv2d_5/kernel:0'     : 'decoder.3.weight',
+        'conv2d_6/conv2d_6/bias:0'       : 'decoder.5.bias',
+        'conv2d_6/conv2d_6/kernel:0'     : 'decoder.5.weight',
+        'sequential_1/conv2d_1/bias:0'   : 'encoder.0.bias',
+        'sequential_1/conv2d_1/kernel:0' : 'encoder.0.weight',
+        'sequential_1/conv2d_2/bias:0'   : 'encoder.2.bias',
+        'sequential_1/conv2d_2/kernel:0' : 'encoder.2.weight',
+        'sequential_1/conv2d_3/bias:0'   : 'encoder.5.bias',
+        'sequential_1/conv2d_3/kernel:0' : 'encoder.5.weight',
+    }
+
+    # Create AE
+    ae = AE(h5_to_state_dict(weight_file, AE_map))
+
+
 
     if print_summary:
-        print("Decoder summaries")
-        decoder.summary()
+        summary(ae, (28,28,1))
 
-    return decoder
+    return ae
+
+ae = load_AE('mnist_AE_weights', True)
+# p = ae.predict(torch.zeros((10,28,28,1)))
+# print(p)
+
 
 def save_img(img, name="output"):
     """Save an MNIST image to location name, both as .pt and .png."""
