f2893cdf780c57d28c4230e237297367d105e585
Mattijs Blankesteijn <25477219+MBWhitestone@users.noreply.github.com>
2020-01-10 17:05:34 +0100

    fista cleanup

--- Pytorch/fista.py
+++ Pytorch/fista.py
@@ -1,34 +1,44 @@
-import sys
+## fista.py -- fast iterative shrinkage thresholding algorithm
+##
+## (C) 2020 UvA FACT AI group
+
 import torch
-import numpy as np
-import torchvision
 
-def fista(label, beta, iter, max_iter, delta, slack, orig_img):
-    cond1 = ((slack - orig_img) > beta).float32()
-    cond2 = ((torch.abs(slack - orig_img)) <= beta).float32()
-    cond3 = ((slack - orig_img) < -beta).float32()
-    upper = torch.min((slack - beta), torch.tensor(0.5, dtype=torch.float32))
-    lower = torch.max((slack + beta), torch.tensor(-0.5, dtype=torch.float32))
+def fista(label, beta, step, delta, slack, orig_img):
+    """
+    Fast Iterative Shrinkage-Thresholding Algorithm.
+    Input:
+        - label    : label for orig_img
+        - beta     : Hyperparameter (0.1)
+        - step     : k
+        - delta    : last perturbation
+        - slack    : last slack vector
+        - orig_img : image from dataset
+    Returns:
+        - delta_update : new perturbation
+        - slack_update : new slack vector
+    """
+    # Delta Update.
+    z = slack - orig_img
+    delta_update = (z > beta) * torch.min((slack - beta), torch.tensor(0.5)) + \
+                   (torch.abs(z) <= beta) * orig_img + \
+                   (z < -beta) * torch.max((slack + beta), torch.tensor(-0.5))
+    delta_update = update(delta_update, orig_img, label)
 
-    delta_update = cond1 * upper + cond2 * orig_img + cond3 * lower
-
-    cond4 = ((delta_update - orig_img) > 0).float32()
-    cond5 = ((delta_update - orig_img) <= 0).float32()
-
-    if label == "PP":
-        delta_update = cond5 * delta_update + cond4 * orig_img
-    elif label == "PN":
-        delta_update = cond4 * delta_update + cond5 * orig_img
-
-    zt = iter / (iter + torch.tensor(3, dtype=torch.float32)))
+    # Slack update.
+    zt = step / (step + torch.tensor(3))
     slack_update = delta_update + zt * (delta_update - delta)
-
-    cond6 = ((slack_update - orig_img) > 0).float32()
-    cond7 = ((slack_update - orig_img) <= 0).float32()
-
-    if label == "PP":
-        slack_update = cond7 * slack_update + cond6 * orig_img
-    elif label == "PN":
-        slack_update = cond6 * slack_update + cond7 * orig_img
+    slack_update = update(slack_update, orig_img, label)
 
     return delta_update, slack_update
+
+def update(variable, orig_img, label):
+    """Update a variable based on label and its difference with orig_img."""
+    z = variable - orig_img
+    if label == "PP":
+        return (z <= 0) * variable + (z > 0) * orig_img
+    elif label == "PN":
+        return (z > 0) * variable + (z <= 0) * orig_img
+
+    # When label is unkown
+    return None
