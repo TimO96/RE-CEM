201ec299c7524aa9270d13d51f914c2b2dfed675
Stefan <stefanklut12@gmail.com>
2020-01-14 15:27:37 +0100

    Conform the decay with the tf implementation

--- Pytorch/polynomial_decay.py
+++ Pytorch/polynomial_decay.py
@@ -1,5 +1,4 @@
-def poly_lr_scheduler(optimizer, init_lr, step, lr_decay_step=1,
-                      max_step=100, power=0.9):
+def poly_lr_scheduler(optimizer, init_lr, step, end_learning_rate=0.0001, lr_decay_step=1, max_step=100000, power=1):
     """
     Polynomial decay of learning rate
     Input:
@@ -16,7 +15,7 @@ def poly_lr_scheduler(optimizer, init_lr, step, lr_decay_step=1,
     if step % lr_decay_step or step > max_step:
         return optimizer
 
-    lr = init_lr*(1 - step/max_step)**power
+    lr = (init_lr - end_learning_rate)*(1 - step/max_step)**power + end_learning_rate
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
