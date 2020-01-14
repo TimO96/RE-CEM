5d924f8eafa74802e3e91f89fa8fcbfdb41941b0
Stefan <stefanklut12@gmail.com>
2020-01-14 13:19:41 +0100

    Renamed loss and added return statements

--- Pytorch/evaluation.py
+++ Pytorch/evaluation.py
@@ -2,7 +2,7 @@
 ##
 ## (C) 2020 UvA FACT AI group
 
-def loss(delta, orig_img, target_lab, kappa, AE, const, beta, to_optimize=True, mode="PN"):
+def loss(delta, orig_img, target_lab, kappa, gamma, AE, const, beta, to_optimize=True, mode="PN"):
     """
     Compute the loss function component for the network to find either
     pertinent positives (PN) or pertinent negatives (PN).
@@ -11,6 +11,7 @@ def loss(delta, orig_img, target_lab, kappa, AE, const, beta, to_optimize=True, 
         - orig_img      : image from dataset
         - target_lab    : label of the to be predicted target class
         - kappa         : confidence parameter
+        - gamma         : constant
         - AE            : autoencoder module
         - const         : regularization coefficient (hyperparameter)
         - beta          : regularization coefficient (hyperparameter)
@@ -26,24 +27,40 @@ def loss(delta, orig_img, target_lab, kappa, AE, const, beta, to_optimize=True, 
     L1_dist = torch.sum(torch.abs(delta), (1,2,3))
     EN_dist = L2_dist + L1_dist * beta
 
-    Loss_Attack = loss_function(delta, orig_img, target_lab, kappa, mode=mode)
+    loss_attack, pred = loss_function(delta, orig_img, target_lab, kappa, mode=mode)
 
     # Sum up the losses.
-    Loss_L1Dist = torch.sum(L1_dist)
-    Loss_L2Dist = torch.sum(L2_dist)
-    Loss_Attack = torch.sum(const * Loss_Attack)
+    loss_L1_dist = torch.sum(L1_dist)
+    loss_L2_dist = torch.sum(L2_dist)
+    loss_attack = torch.sum(const * loss_attack)
 
     if mode == "PP":
-        Loss_AE_Dist   = gamma * (torch.norm(AE(delta) - delta)**2)
+        loss_AE_dist   = gamma * (torch.norm(AE(delta) - delta)**2)
     elif mode == "PN":
-        Loss_AE_Dist   = gamma * (torch.norm(AE(delta + orig_img) - delta + orig_img)**2)
+        loss_AE_dist   = gamma * (torch.norm(AE(delta + orig_img) - delta + orig_img)**2)
 
     if to_optimize:
-        return Loss_Attack + Loss_L2Dist + Loss_AE_Dist
+        loss = loss_attack + loss_L2_dist + loss_AE_dist
     else:
-        return Loss_Attack + Loss_L2Dist + Loss_AE_Dist + torch.mul(beta, Loss_L1Dist)
+        loss = loss_attack + loss_L2_dist + loss_AE_dist + loss_L1_dist * beta
+
+    return loss, EN_dist, pred
 
 def loss_function(delta, orig_img, target_lab, kappa, mode="PN"):
+    """
+    Compute the loss function component for the network to find either
+    pertinent positives (PN) or pertinent negatives (PN).
+    Input:
+        - delta         : last perturbation
+        - orig_img      : image from dataset
+        - target_lab    : label of the to be predicted target class
+        - kappa         : confidence parameter
+        - mode          : find either PP or PN
+    Returns:
+        - computed loss between the most probable class and the most probable
+          class given the pertubation (delta) without regularizers.
+    """
+
     # Prediction BEFORE-SOFTMAX of the model.
     if mode == "PP":
         pred = model.predict(delta)
@@ -51,10 +68,12 @@ def loss_function(delta, orig_img, target_lab, kappa, mode="PN"):
         pred = model.predict(orig_img + delta)
 
     # Compute the probability of the label class versus the maximum other.
-    target_lab_score = torch.sum((target_lab) * pred, 1)
+    target_lab_score = torch.sum((target_lab) * pred, dim=1)
     max_nontarget_lab_score = torch.max((1-target_lab) * pred - (target_lab*10000), dim=1)
 
     if mode == "PP":
-        Loss_Attack = torch.max(0.0, max_nontarget_lab_score - target_lab_score + kappa)
+        loss_attack = torch.max(0.0, max_nontarget_lab_score - target_lab_score + kappa)
     elif mode == "PN":
-        Loss_Attack = torch.max(0.0, -max_nontarget_lab_score + target_lab_score + kappa)
+        loss_attack = torch.max(0.0, -max_nontarget_lab_score + target_lab_score + kappa)
+
+    return loss_attack, pred
