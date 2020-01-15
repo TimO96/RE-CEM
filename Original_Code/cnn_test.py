c38934ffc19966788800c8110069c0e52dc97289
Stefan <stefanklut12@gmail.com>
2020-01-15 17:25:51 +0100

    plot images as sanity check

--- Original_Code/cnn_test.py
+++ Original_Code/cnn_test.py
@@ -1,14 +1,20 @@
 from setup_mnist import MNIST, MNISTModel
+import numpy as np
 import tensorflow as tf
+import matplotlib.pylab as plt
 
 mnist = MNIST()
 
+plt.imshow(np.squeeze(mnist.test_data[4]))
+plt.show()
+
 with tf.Session() as sess:
     cnn = MNISTModel(restore="models/mnist", session=sess)
+    
     pred = cnn.predict(mnist.test_data).eval().argmax(axis=1)
     label = mnist.test_labels.argmax(axis=1)
-    for var in tf.all_variables():
-        print(var.eval())
+    # for var in tf.all_variables():
+    #    print(var.eval())
 
 
 acc = (pred == label).mean()
--- Pytorch/cnn_test.py
+++ Pytorch/cnn_test.py
@@ -1,14 +1,21 @@
 from setup_mnist import MNIST, MNISTModel
+import numpy as np
+import matplotlib.pylab as plt
 
 mnist = MNIST()
+
+plt.imshow(np.squeeze(mnist.test_data[4]))
+plt.show()
+
 cnn = MNISTModel(restore="models/mnist.pt")
 
 pred = cnn.predict(mnist.test_data).argmax(dim=1)
 label = mnist.test_labels.argmax(dim=1)
+print(label)
 
 acc = (pred == label).float().mean()
 
-for name, param in cnn.named_parameters():
-    print(name, param)
+# for name, param in cnn.named_parameters():
+#     print(name, param)
 
 print(acc)
