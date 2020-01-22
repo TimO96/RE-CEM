18acb6a0a69f30afda45df5602324cd27919d279
Mattijs Blankesteijn <25477219+MBWhitestone@users.noreply.github.com>
2020-01-22 23:09:37 +0100

    beautifying main

--- Pytorch/main.py
+++ Pytorch/main.py
@@ -1,36 +1,26 @@
-## main.py -- sample code to test attack procedure
-##
+## main.py -- implements Main class controlling all others.e
+
+## (C) 2020 Changes by UvA FACT AI group [Pytorch conversion]
+
+## Based on:
 ## Copyright (C) 2018, IBM Corp
 ##                     Chun-Chen Tu <timtu@umich.edu>
 ##                     PaiShun Ting <paishun@umich.edu>
 ##                     Pin-Yu Chen <Pin-Yu.Chen@ibm.com>
-##
-## Licensed under the Apache License, Version 2.0 (the "License");
-## you may not use this file except in compliance with the License.
-## You may obtain a copy of the License at
-##
-##     http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software
-## distributed under the License is distributed on an "AS IS" BASIS,
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-## See the License for the specific language governing permissions and
-## limitations under the License.
-
-## (C) 2020 Changes by UvA FACT AI group [Pytorch conversion]
 
 import os
 import sys
 import random
 import time
-from setup_mnist import MNIST, MNISTModel
-import torch
-from torch import cuda, manual_seed
-from torch.backends import cudnn
-import utils as util
-from CEM import CEM
 import matplotlib.pyplot as plt
 from torchsummary import summary
+from torch import cuda, manual_seed, load
+from torch import abs as torchabs
+from torch.backends import cudnn
+
+from setup_mnist import MNIST, MNISTModel
+import utils as util
+from CEM import CEM
 
 class Main:
     def __init__(self, mode="PN", max_iter=10, kappa=10, beta=1e-1, gamma=100,
@@ -63,8 +53,8 @@ class Main:
 
         #Load autoencoder and MNIST dataset.
         # AE_model = util.load_AE("mnist_AE_weights").to(dvc)
-        self.ae = util.AE(torch.load(model_dir+ae, map_location=dvc)).to(dvc)
-        self.nn = MNISTModel(torch.load(model_dir+nn, map_location=dvc)).to(dvc)
+        self.ae = util.AE(load(model_dir+ae, map_location=dvc)).to(dvc)
+        self.nn = MNISTModel(load(model_dir+nn, map_location=dvc)).to(dvc)
         self.data = data(dvc)
         self.mode = mode
         self.kappa = kappa
@@ -106,7 +96,7 @@ class Main:
         # Calculate probability classes for adversarial and delta image.
         self.adv_pred, self.adv_label, self.adv_str = self.prediction(self.adv)
         self.delta_pred, self.delta_label, self.delta_str = self.prediction(delta)
-        self.delta = torch.abs(delta)-0.5
+        self.delta = torchabs(delta)-0.5
 
     def report(self):
         """Print report."""
@@ -116,42 +106,43 @@ class Main:
             time = 'None'
 
         INFO = f"\n\
-                 [INFO]\n\
-                 id:          {self.id}                           \n\
-                 mode:        {self.mode}                         \n\
-                 time (s):    {time}                              \n\
-                 kappa:       {self.kappa}                        \n\
-                 Original:    {self.label} {self.str}             \n\
-                 Delta:       {self.delta_label} {self.delta_str} \n\
-                 Adversarial: {self.adv_label} {self.adv_str}     \n"
+        [INFO]\n\
+        id:          {self.id}                           \n\
+        mode:        {self.mode}                         \n\
+        time (s):    {time}                              \n\
+        kappa:       {self.kappa}                        \n\
+        Original:    {self.label} {self.str}             \n\
+        Delta:       {self.delta_label} {self.delta_str} \n\
+        Adversarial: {self.adv_label} {self.adv_str}     \n"
         print(INFO)
 
-    def store_images(self):
+    def store_images(s):
         """Store images to s.store directory."""
-        suffix = f"id{self.id}_Orig{self.label}_Adv{self.adv_label}_Delta{self.delta_label}"
-        save = f"{self.store}/{self.mode}_ID{self.id}_Gamma_{self.gamma}_Kappa_{self.kappa}"
-        os.system(f"mkdir -p {save}")
+        sfx = f"id{s.id}_Orig{s.label}_Adv{s.adv_label}_Delta{s.delta_label}"
+        dir = f"{s.store}/{s.mode}_ID{s.id}_Gamma_{s.gamma}_Kappa_{s.kappa}"
+        os.system(f"mkdir -p {dir}")
 
-        self.img_pic = util.save_img(self.img, f"{save}/Orig_{self.label}")
-        self.delta_pic = util.save_img(self.delta, f"{save}/Delta_{suffix}", self.mode)
-        self.adv_pic = util.save_img(self.img, f"{save}/Adv_{suffix}", self.mode, mode_img=self.delta)
+        s.img_pic = util.save_img(s.img, f"{dir}/Orig_{s.label}")
+        s.delta_pic = util.save_img(s.delta, f"{dir}/Delta_{sfx}", s.mode)
+        s.adv_pic = util.save_img(s.img, f"{dir}/Adv_{sfx}", s.mode, s.delta)
 
     def show_images(self, w=18.5, h=10.5):
         """Show img, delta and adv next to each other."""
         f, ax = plt.subplots(1,3)
         f.set_size_inches(w, h)
         ax[0].imshow(self.img_pic, cmap='gray', vmin=0, vmax=255)
-        ax[2].imshow(self.delta_pic)
-        ax[1].imshow(self.adv_pic)
+        ax[1].imshow(self.delta_pic)
+        ax[2].imshow(self.adv_pic)
         plt.show()
 
     def summary(self):
+        """Print information on the model and ae."""
         shape = (self.ae.image_size, self.ae.image_size, self.ae.num_channels)
-        summary(self.ae, shape)
-        summary(self.nn, shape)
 
-        print(str(self.nn))
         print(str(self.ae))
+        summary(self.ae, shape)
+        print(str(self.nn))
+        summary(self.nn, shape)
 
     def run(self, id=2952, show=True):
         """Run the algorithm for specific image."""
@@ -164,8 +155,8 @@ class Main:
         if show:
             self.show_images()
 
-pp = Main(mode='PP')
-pp.summary()
+# pp = Main(mode='PP')
 # pn = Main(mode='PN')
+# pp.summary()
 # pp.run(1234)
 # pn.run(1234)
