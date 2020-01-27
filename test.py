f518f75432b780dda7bccc570d13b49678fd5ac7
Mattijs Blankesteijn <25477219+MBWhitestone@users.noreply.github.com>
2020-01-27 17:00:17 +0100

    array figs

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 #from cem.main import Main
 # m = Main(mode='PP')
 import cem
-cem.Main(mode='PP', type='FMNIST').explain(10, gamma=0, kappa=10, max_iter=1000)
+# cem.Main(mode='PP', type='FMNIST').explain(10, gamma=0, kappa=10, max_iter=1000)
+
+cem.Main(type='MNIST').show_array(10, kappa=10, max_iter=10)
