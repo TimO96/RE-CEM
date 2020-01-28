9541c13bc0f9fd951810c8dc2a417775d49f6b8b
Stefan <stefanklut12@gmail.com>
2020-01-28 17:11:43 +0100

    quantative evaluation on lisa?

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 ## (C) 2020 UvA FACT AI
 
 import cem
-cem.Main(type='MNIST').explain(2953, mode='PP', gamma=0, kappa=10, max_iter=1000)
+m = cem.Main(seed=121, type='MNIST')
+
+print(m.quant_eval(ids=list(range(100))))
+
+
